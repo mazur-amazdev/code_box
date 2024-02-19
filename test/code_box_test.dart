90178f9b75798712926a79091e5522f5d83cd2b8
Julian Mazur <j.mazur@amazdev.de>
2024-02-19 13:33:38 +0100

    add the list extensions and update tests

--- test/code_box_test.dart
+++ test/code_box_test.dart
@@ -1,12 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:code_box/code_box.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('check if the list can be flatmapped', () {
+    final list = [
+      null,
+      "test",
+      null,
+      "test1",
+      "test2",
+      null,
+      ["test3", "test4", 5]
+    ];
+    final flatmapped = list.flatMap<String>();
+
+    expect(flatmapped, ["test", "test1", "test2", "test3", "test4"]);
+  });
+  test('check if the list could be uniqued by its type', () {
+    final list = ["test", "test", "test1", "test2", "test4", "test1", "test2"];
+    final List<String> uniqued = list.unique();
+    expect(uniqued, ["test", "test1", "test2", "test4"]);
   });
 }
