bd0e2f89a9081897bd94b1433ebecc825cd84cf3
Julian Mazur <j.mazur@amazdev.de>
2024-02-19 14:14:05 +0100

    add an example and clean up pubspec

---
+++ example/code_box_example.dart
@@ -0,0 +1,34 @@
+import 'package:code_box/code_box.dart';
+import 'package:code_box/src/CodeBoxWidgetModifier.dart';
+import 'package:flutter/material.dart';
+
+final class TestWidget extends StatelessWidget {
+  TestWidget({super.key, required this.injected});
+
+  // MARK: Properties
+
+  List injected;
+
+  // MARK: build
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+            children: injected
+                .unique()
+
+                /// we only want each entry once
+                /// just want to show the strings from the injected list
+                .map((e) {
+                  (e != String) ? null : Text(e);
+                })
+
+                /// make the map to list
+                .toList()
+
+                /// and flat map it to the wanted list type
+                .flatMap())
+        .padding(top: 15)
+        .safeArea();
+  }
+}
