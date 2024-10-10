95a7fc1958af76d9f6a0e68bfc2eb47dd0338397
Julian Mazur <j.mazur@amazdev.de>
2024-10-10 21:05:33 +0200

    fix shows

--- lib/code_box.dart
+++ lib/code_box.dart
@@ -1,6 +1,6 @@
 library code_box;
 
-export 'src/CodeBoxList.dart' show CodeBoxList;
+export 'src/CodeBoxList.dart' show CodeBoxList, IdentifiableCodeBoxList;
 export 'src/CodeBoxWidgetModifier.dart' show CodeBoxWidgetModifier;
 export 'src/CodeBoxIdentifiable.dart' show Identifiable;
 export 'src/CodeBoxSet.dart' show CodeBoxSet;
